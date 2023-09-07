67d0819f9c865eb528f7e6034246ae3560513636
Muhammad Taaha <96954628+TSTRIVER@users.noreply.github.com>
2023-09-07 16:32:10 +0530

    Delete Profile.js

--- Profile.js
+++ Profile.js
@@ -1,8 +1,1 @@
-import React from 'react'
-import { Alert, StyleSheet, Text, View, Button } from "react-native";
 
-const Profile = ({route}) => {
-    return <Text>This is {route.params.name}'s profile</Text>;
-}
-
-export default Profile
